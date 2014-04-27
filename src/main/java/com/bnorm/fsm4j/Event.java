a6340586716ebe5738d4940435a52e424edf38cc
Brian Norman <brian.e.norman@gmail.com>
2014-04-27 17:14:39 -0500

    Created base interface for all state machine events

---
+++ src/main/java/com/bnorm/fsm4j/Event.java
@@ -0,0 +1,10 @@
+package com.bnorm.fsm4j;
+
+/**
+ * Base interface for all events.  This interface provides no functionality but does provide a common parent interface
+ * for all state machine events.
+ *
+ * @author Brian Norman
+ */
+public interface Event {
+}
