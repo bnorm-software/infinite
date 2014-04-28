8c921d1323100f7aa2c9df224240e7bfbe65a62d
Brian Norman <brian.e.norman@gmail.com>
2014-04-28 16:30:55 -0500

    Created the interface for entrance and exit actions

---
+++ src/main/java/com/bnorm/fsm4j/Action.java
@@ -0,0 +1,22 @@
+package com.bnorm.fsm4j;
+
+/**
+ * Functional interface that represents an action that should be performed.  Actions are performed when a state is
+ * either entered or exited.
+ *
+ * @author Brian Norman
+ */
+@FunctionalInterface
+public interface Action<S extends State, E extends Event> {
+
+    /**
+     * Performs a specific action.  The parameters specified are the conditions of the state transition.  Sometimes the
+     * state being entered or existed is not a part of the specific transition - it might be a parent state of the
+     * source or destination.
+     *
+     * @param state the state being entered or exited.
+     * @param event the event causing the transition.
+     * @param transition the exact transition taking place.
+     */
+    void perform(S state, E event, Transition<S> transition);
+}
