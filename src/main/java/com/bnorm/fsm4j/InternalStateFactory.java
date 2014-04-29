bf6c902116054b30d03a0424ada1829f7662cd0a
Brian Norman <brian.e.norman@gmail.com>
2014-04-28 23:00:42 -0500

    Created factory interface for creating internal states

---
+++ src/main/java/com/bnorm/fsm4j/InternalStateFactory.java
@@ -0,0 +1,30 @@
+package com.bnorm.fsm4j;
+
+/**
+ * A factory interface for internal states.
+ *
+ * @author Brian Norman
+ * @version 1.0
+ * @since 1.0
+ */
+public interface InternalStateFactory {
+
+    /**
+     * Returns the default internal state factory.  This is the internal state base constructor.
+     *
+     * @return default internal state factory.
+     */
+    static InternalStateFactory createDefault() {
+        return InternalStateBase::new;
+    }
+
+    /**
+     * Creates a new internal state from specified state.
+     *
+     * @param state the state to wrap.
+     * @param <S> the class type of the states.
+     * @param <E> the class type of the events.
+     * @return a new internal state.
+     */
+    <S extends State, E extends Event> InternalState<S, E> create(S state);
+}
