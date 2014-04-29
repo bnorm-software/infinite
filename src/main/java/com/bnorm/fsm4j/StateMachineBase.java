6f3dec824c4cb3ba5002463d5e4daeb941824c7c
Brian Norman <brian.e.norman@gmail.com>
2014-04-28 22:40:55 -0500

    Created base implementation for a state machine

---
+++ src/main/java/com/bnorm/fsm4j/StateMachineBase.java
@@ -0,0 +1,71 @@
+package com.bnorm.fsm4j;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The base implementation of a state machine.
+ *
+ * @author Brian Norman
+ * @version 1.0
+ * @since 1.0
+ */
+class StateMachineBase<S extends State, E extends Event> implements StateMachine<S, E> {
+
+    /** The state machine transition listeners. */
+    private final Set<TransitionListener<S, E>> listeners;
+
+    /** The state to internal state map. */
+    private final Map<S, InternalState<S, E>> states;
+
+    /** The event to transition map. */
+    private final Map<E, Set<Transition<S>>> transitions;
+
+    /** The current state of the state machine. */
+    private S state;
+
+    /**
+     * Constructs a new state machine from the specified state map, transition map, and starting state.
+     *
+     * @param states the states of the state machine.
+     * @param transitions the transitions of the state machine.
+     * @param starting the starting state of the state machine.
+     */
+    StateMachineBase(Map<S, InternalState<S, E>> states, Map<E, Set<Transition<S>>> transitions, S starting) {
+        this.listeners = new HashSet<>();
+        this.states = states;
+        this.transitions = transitions;
+        setState(starting);
+    }
+
+    @Override
+    public void setState(S state) {
+        this.state = state;
+    }
+
+    @Override
+    public S getState() {
+        return state;
+    }
+
+    @Override
+    public InternalState<S, E> getInternalState(S state) {
+        return states.get(state);
+    }
+
+    @Override
+    public Set<Transition<S>> getTransitions(E event) {
+        return transitions.computeIfAbsent(event, e -> new HashSet<>());
+    }
+
+    @Override
+    public Set<TransitionListener<S, E>> getTransitionListeners() {
+        return listeners;
+    }
+
+    @Override
+    public void addTransitionListener(TransitionListener<S, E> listener) {
+        listeners.add(listener);
+    }
+}
