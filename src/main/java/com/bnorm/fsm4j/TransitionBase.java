7e7f26d27c5798569d5f3ffdda1b73f78a1ddcea
Brian Norman <brian.e.norman@gmail.com>
2014-04-28 22:35:27 -0500

    Created base implementation for a transition

---
+++ src/main/java/com/bnorm/fsm4j/TransitionBase.java
@@ -0,0 +1,43 @@
+package com.bnorm.fsm4j;
+
+/**
+ * The base implementation of a transition.
+ *
+ * @author Brian Norman
+ * @version 1.0
+ * @since 1.0
+ */
+class TransitionBase<S extends State> implements Transition<S> {
+
+    /** The source state of the transition. */
+    private final S source;
+
+    /** The destination state of the transition. */
+    private S destination;
+
+    /**
+     * Constructs a new transition from the specified source and destination states.
+     *
+     * @param source the source state of the transition.
+     * @param destination the destination state of the transition.
+     */
+    TransitionBase(S source, S destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    @Override
+    public S getSource() {
+        return source;
+    }
+
+    @Override
+    public S getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String toString() {
+        return "TransitionBase[" + source + "->" + destination + "]";
+    }
+}
