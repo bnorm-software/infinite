3d785d42e4219e37d15825b95b7c99275ad3a19d
Brian Norman <brian.e.norman@gmail.com>
2014-12-13 18:58:32 -0600

    File state machine structure and structure factory
    The file state machine structure factory is used to load state machine
    structures from a text file.  This is useful if the state machine needs
    to be defined outside of the program.

---
+++ src/main/java/com/bnorm/infinite/file/FileStateMachineStructureFactory.java
@@ -0,0 +1,49 @@
+package com.bnorm.infinite.file;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.bnorm.infinite.InternalStateFactory;
+import com.bnorm.infinite.StateMachineException;
+import com.bnorm.infinite.StateMachineStructure;
+import com.bnorm.infinite.StateMachineStructureFactory;
+import com.bnorm.infinite.TransitionFactory;
+
+/**
+ * A factory for creating state machine structures for a file.
+ *
+ * @param <S> the class type of the states.
+ * @param <E> the class type of the events.
+ * @param <C> the class type of the context.
+ * @author Brian Norman
+ * @since 1.2.0
+ */
+public class FileStateMachineStructureFactory<S, E, C> implements StateMachineStructureFactory<S, E, C> {
+
+    /** File location of the state machine text. */
+    private final Path path;
+
+    /** Reader used to interpret the state machine text. */
+    private final StringStateMachineReader<S, E, C> stateMachineReader;
+
+    /**
+     * Constructors a new FileStateMachineStructureFactory with the specified parameters.
+     *
+     * @param path the file location of the state machine text.
+     * @param stateMachineReader the reader used to load the state machine.
+     */
+    public FileStateMachineStructureFactory(Path path, StringStateMachineReader<S, E, C> stateMachineReader) {
+        this.path = path;
+        this.stateMachineReader = stateMachineReader;
+    }
+
+    @Override
+    public StateMachineStructure<S, E, C> create(InternalStateFactory<S, E, C> internalStateFactory,
+                                                 TransitionFactory<S, C> transitionFactory) {
+        try {
+            return new FileStateMachineStructure<>(internalStateFactory, transitionFactory, path, stateMachineReader);
+        } catch (IOException e) {
+            throw new StateMachineException("Unable to read specified path [" + path + "]", e);
+        }
+    }
+}
